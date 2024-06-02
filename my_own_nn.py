fdd6b81dacf52028a3ad58687695b846d60afb11
Farlon Souto <farlonsouto@gmail.com>
2024-06-02 22:51:22 +0200

    Comes up with some graphics, but way too statically

--- my_own_nn.py
+++ my_own_nn.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv1D, Flatten
 from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+import matplotlib.pyplot as plt
 
 
 # Function to load and preprocess data from the HDF5 file
@@ -36,6 +37,7 @@ def load_data(filepath, building, start_time, end_time):
 
         return df
 
+
 # Load training and testing data
 train_mains = load_data('ukdale.h5', 1, '2014-01-01', '2015-02-15')
 test_mains = load_data('ukdale.h5', 5, '2014-01-01', '2015-02-15')
@@ -44,6 +46,7 @@ test_mains = load_data('ukdale.h5', 5, '2014-01-01', '2015-02-15')
 max_power = train_mains['power'].max()
 train_mains['power'] = train_mains['power'] / max_power
 test_mains['power'] = test_mains['power'] / max_power
+
 
 # Function to create the neural network model
 def create_model(input_shape_param):
@@ -55,6 +58,7 @@ def create_model(input_shape_param):
     model.add(Dense(1, activation='linear'))
     model.compile(optimizer='adam', loss='mse', metrics=['mae'])
     return model
+
 
 # Define parameters for the TimeseriesGenerator
 window_size = 60
@@ -79,5 +83,22 @@ model = create_model(input_shape)
 model.fit(train_generator, epochs=10, validation_data=test_generator)
 
 # Evaluate the model on the test data
-loss, mae = model.evaluate(test_generator)
+loss, mae = model.evaluate(test_generator, verbose=0)
 print(f'Test MAE: {mae}')
+
+# Get the predictions for each window in the test data
+predictions = model.predict(test_generator)
+
+# Plot original aggregated power consumption
+plt.figure(figsize=(10, 6))
+plt.plot(test_mains.index, test_mains['power'], label='Original Aggregate Power', color='blue')
+
+# Plot predicted power consumption for each appliance
+for i in range(predictions.shape[1]):
+    plt.plot(test_mains.index[window_size:], predictions[:, i], label=f'Predicted Appliance {i+1}', alpha=0.7)
+
+plt.title('Disaggregated Power Consumption')
+plt.xlabel('Timestamp')
+plt.ylabel('Power')
+plt.legend()
+plt.show()
