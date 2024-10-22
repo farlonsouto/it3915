a502c19ca9ab37e0a161582827dcd54cba1eaf04
Farlon Souto <farlonsouto@gmail.com>
2024-10-22 18:52:49 +0200

    Adds a decoder so the Transformer has the seq2seq behavior expected for the NILM domain ref:IT3920

--- src/transformer.py
+++ src/transformer.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.layers import Input, Dense, LayerNormalization, MultiHeadAttention, Dropout, Add, \
     GlobalAveragePooling1D
 from tensorflow.keras.models import Model
+from tensorflow.python.keras.layers import TimeDistributed
 
 
 class Transformer:
@@ -27,16 +28,39 @@ class Transformer:
 
         return ff_output
 
-    def create_transformer_model(self, metrics=None):
+    def transformer_decoder(self, inputs, encoder_output):
+        attention_output = MultiHeadAttention(num_heads=self.num_heads, key_dim=self.head_size, dropout=self.dropout)(
+            inputs, encoder_output)
+        attention_output = Add()([inputs, attention_output])
+        attention_output = LayerNormalization(epsilon=1e-6)(attention_output)
+
+        ff_output = Dense(self.ff_dim, activation='relu')(attention_output)
+        ff_output = Dropout(self.dropout)(ff_output)
+        ff_output = Dense(inputs.shape[-1])(ff_output)
+
+        ff_output = Add()([attention_output, ff_output])
+        ff_output = LayerNormalization(epsilon=1e-6)(ff_output)
+
+        return ff_output
+
+    def create_transformer_model(self, metrics=None, num_appliances=None):
         inputs = Input(shape=self.shape_of_input)
         x = inputs
 
+        # Encoder
         for _ in range(self.num_transformer_blocks):
             x = self.transformer_encoder(x)
 
+        # Decoder
+        decoder_inputs = Input(shape=(None, self.shape_of_input[-1]))  # Adjust shape as necessary
+        for _ in range(self.num_transformer_blocks):
+            x = self.transformer_decoder(decoder_inputs, x)
+
+        # Final layers
         x = GlobalAveragePooling1D()(x)
         x = Dense(64, activation='relu')(x)
-        outputs = Dense(1, activation='linear')(x)
+        outputs = TimeDistributed(Dense(num_appliances, activation='linear')(x))  # Adjust for number of appliances
 
-        model = Model(inputs, outputs)
+        model = Model([inputs, decoder_inputs], outputs)
         return model
+
