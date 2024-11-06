d948ba0a3c693ccc0c0d15fa98fff28c9ac329c0
Farlon Souto <farlonsouto@gmail.com>
2024-11-06 16:03:53 +0100

    Setup check and gitignore improved ref:IT3920

--- src/setup_check.py
+++ src/setup_check.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 from tensorflow.python.platform import build_info as tf_build_info
 
+
+print("Tensorflow version: ",tf.__version__)
+print("Keras version: ", tf.keras.__version__)
+
+
 print(tf_build_info.build_info)
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
