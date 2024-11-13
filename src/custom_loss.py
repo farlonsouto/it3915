9697bed2e2fb63d3892d88218469060b5925700b
Farlon Souto <farlonsouto@gmail.com>
2024-11-13 11:35:20 +0100

    Adds new loss norm + wighted function ref:IT3920

--- src/custom_loss.py
+++ src/custom_loss.py
@@ -1,6 +1,4 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.python.keras.losses import KLDivergence
 
 
 def nde_loss(y_true, y_pred):
@@ -32,51 +30,60 @@ def nde_loss(y_true, y_pred):
     return tf.clip_by_value(total_loss, 0.0, 1e6)
 
 
-def bert4nilm_loss(y: tuple, s: tuple):
+def bert4nilm_loss(power_readings: tuple, state_readings: tuple):
     """
     Custom loss function as per the equation provided by the paper 'BERT4NILM: A Bidirectional Transformer Model for
     Non-Intrusive Load Monitoring - Zhenrui Yue et. al'.
 
     Arguments:
-        y -- tuple of (Ground truth values for x, Predicted values for x (model's output)).
-        s -- tuple of (Ground truth on/off state, Predicted on/off state).
+        power_readings -- tuple of (Ground truth values for x, Predicted values for x (model's output)).
+        state_readings -- tuple of (Ground truth on/off state, Predicted on/off state).
 
     Returns:
     loss -- The computed loss value.
     """
 
     # Assigning values and ensuring same shapes
-    y_ground_truth, y_predicted = y
-    y_predicted = tf.reshape(y_predicted, tf.shape(y_ground_truth))
+    app_pw_grd_truth, app_pw_predicted = power_readings
+    app_pw_predicted = tf.reshape(app_pw_predicted, tf.shape(app_pw_grd_truth))
 
-    s_ground_truth, s_predicted = s
-    s_predicted = tf.reshape(s_predicted, tf.shape(s_ground_truth))
+    app_on_off_state_grd_truth, app_on_off_state_predicted = state_readings
+    app_on_off_state_predicted = tf.reshape(app_on_off_state_predicted, tf.shape(app_on_off_state_grd_truth))
 
     tau = 1.0
-    lambda_val = 0.1
+    # lambda_val = 0.1
 
     # Mean Squared Error (MSE)
-    mse_loss = tf.reduce_mean(tf.square(y_predicted - y_ground_truth))
+    mse_loss = tf.reduce_mean(tf.square(app_pw_predicted - app_pw_grd_truth))
 
-    # Kullback-Leibler Divergence between softmax distributions
-    softmax_true = tf.nn.softmax(y_ground_truth / tau)
-    softmax_pred = tf.nn.softmax(y_predicted / tau)
-    
+    # Kullback-Leibler Divergence (KLD) between softmax distributions
+    softmax_true = tf.nn.softmax(app_pw_grd_truth / tau)
+    softmax_pred = tf.nn.softmax(app_pw_predicted / tau)
+
     # Original code for 1 GPU or 1 CPU
     # kl_loss = KLDivergence()(softmax_true, softmax_pred)
 
-    #Code to handle reduction for multiple GPUs:
+    # Code to handle reduction into the multiple GPUs scenario:
     # Modify the KL divergence calculation
-    kl_loss = tf.reduce_sum(tf.keras.losses.KLDivergence(reduction=tf.keras.losses.Reduction.NONE)(softmax_true, softmax_pred))
+    kl_diverg = tf.reduce_sum(
+        tf.keras.losses.KLDivergence(reduction=tf.keras.losses.Reduction.NONE)(softmax_true, softmax_pred))
 
     # Log-sigmoid cross-entropy term
-    log_sigmoid_loss = tf.reduce_mean(tf.math.log(1 + tf.exp(-s_predicted * s_ground_truth)))
+    log_sigmoid_loss = tf.reduce_mean(tf.math.log(1 + tf.exp(-app_on_off_state_predicted * app_on_off_state_grd_truth)))
 
     # L1-norm (MAE) over a subset O (assuming the subset O is all data points here)
-    l1_loss = tf.reduce_mean(tf.abs(y_predicted - y_ground_truth))
+    l1_loss = tf.reduce_mean(tf.abs(app_pw_predicted - app_pw_grd_truth))
 
-    # The complete formula
-    total_loss = mse_loss + kl_loss + log_sigmoid_loss + lambda_val * l1_loss
+    # The complete original formula:
+    # total_loss = mse_loss + kl_diverg + log_sigmoid_loss + lambda_val * l1_loss
+
+    norm_mse = (mse_loss / 1 + mse_loss)
+    norm_l1_loss = (l1_loss / 1 + l1_loss)
+    norm_kl_diverg = (kl_diverg / 1 + kl_diverg)
+    norm_log_sigm_loss = (log_sigmoid_loss / 1 + log_sigmoid_loss)
+
+    # The suggested variation where the terms were normalized and the term regarding the appliance state domains
+    # the function
+    total_loss = norm_mse + norm_l1_loss + norm_kl_diverg + (3 * norm_log_sigm_loss)
 
     return total_loss
-
