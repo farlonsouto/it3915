bcdd882b74645b8979f50e9318d67d6e5724e830
Farlon Souto <farlonsouto@gmail.com>
2024-12-03 22:34:10 +0100

    Using tf 2.8.2 packages ref:IT3920

--- dev-tools/setup_check.py
+++ dev-tools/setup_check.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 from tensorflow.python.platform import build_info as tf_build_info
 
-
-print("Tensorflow version: ",tf.__version__)
+print("Tensorflow version: ", tf.__version__)
 print("Keras version: ", tf.keras.__version__)
-
 
 print(tf_build_info.build_info)
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
