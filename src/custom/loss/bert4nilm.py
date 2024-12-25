Stable and running for all model. Masking fixed ref:IT3920

src/custom/loss/bert4nilm.py
@@ -1,53 +1,40 @@
+from abc import ABC
+
 import tensorflow as tf
 
 
-class LossFunction:
+class LossFunction(tf.keras.losses.Loss, ABC):
     def __init__(self, config):
+        super().__init__()
         self.temperature = float(config.temperature)
         self.on_threshold = int(config.on_threshold)
         self.lambda_val = float(config.lambda_val)
 
-    def __call__(self, y_true, y_pred, mask):
-        """
-        Compute masked loss without modifying `y_true` or `y_pred`.
-
-        Args:
-            y_true: Ground truth values, shape (batch_size, window_size, features).
-            y_pred: Predicted values, shape (batch_size, window_size, features).
-            mask: Boolean mask with shape (batch_size, window_size).
-                  True indicates positions to compute loss for.
-
-        Returns:
-            Scalar loss value.
-        """
+    def __call__(self, y_true, y_pred, sample_weight=None):
         eps = 1e-7
 
-        # Ensure the mask is float32 for mathematical operations
-        mask = tf.cast(mask, tf.float32)
-
-        # Reshape mask to match y_true and y_pred
-        mask = tf.expand_dims(mask, axis=-1)  # Shape: (batch_size, window_size, 1)
-
         # MSE term
-        mse = tf.reduce_sum(tf.square(y_pred - y_true) * mask) / (tf.reduce_sum(mask) + eps)
+        mse = tf.reduce_mean(tf.square(y_pred - y_true))
 
         # KL divergence term
         y_true_dist = tf.nn.softmax(y_true / self.temperature + eps, axis=-1)
         y_pred_dist = tf.nn.softmax(y_pred / self.temperature + eps, axis=-1)
-        kl_div = tf.reduce_sum(
-            tf.keras.losses.KLDivergence()(y_true_dist, y_pred_dist) * mask
-        ) / (tf.reduce_sum(mask) + eps)
+        kl_div = tf.reduce_mean(tf.keras.losses.KLDivergence()(y_true_dist, y_pred_dist))
 
         # Binary cross-entropy term
         status_true = tf.cast(y_true > self.on_threshold, tf.float32)
         status_pred = tf.clip_by_value(tf.cast(y_pred > self.on_threshold, tf.float32), eps, 1.0 - eps)
-        bce = tf.reduce_sum(
-            tf.keras.losses.binary_crossentropy(status_true, status_pred) * mask
-        ) / (tf.reduce_sum(mask) + eps)
+        bce = tf.reduce_mean(tf.keras.losses.binary_crossentropy(status_true, status_pred))
 
         # L1 term
-        l1 = tf.reduce_sum(tf.abs(y_pred - y_true) * mask) / (tf.reduce_sum(mask) + eps)
+        l1 = tf.reduce_mean(tf.abs(y_pred - y_true))
 
-        # Total loss
+        # Debug prints
+        # tf.print("\nDebug values:")
+        # tf.print("MSE:", mse)
+        # tf.print("KL:", kl_div)
+        # tf.print("BCE:", bce)
+        # tf.print("L1:", l1)
+
         total_loss = mse + 0.1 * kl_div + 0.1 * bce + 0.01 * self.lambda_val * l1
         return total_loss

src/data/generator.py
@@ -100,7 +100,7 @@ class TimeSeriesDataGenerator(Sequence):
                 stride = self.window_size
                 if self.is_training:
                     stride = self.window_stride
-                for i in range(0, len(mains_power) - self.window_size + 1, stride):
+                for i in range(0, len(aggregated_proc) - self.window_size + 1, stride):
                     yielded_agg = aggregated_proc[i:i + self.window_size]
                     yielded_app = appliance_proc[i:i + self.window_size]
                     yielded_msk = mask_proc[i:i + self.window_size]
@@ -136,9 +136,9 @@ class TimeSeriesDataGenerator(Sequence):
         aggregated, appliance_power = aggregated.align(appliance_power, join='inner', method='pad', limit=1)
 
         # mask_observer.py for values to ignore
-        mask = appliance_power == 1.0
+        standby_power_mask = appliance_power == 1.0
         # Apply clipping only where the mask is False
-        appliance_power = appliance_power.where(mask,
+        appliance_power = appliance_power.where(standby_power_mask,
                                                 appliance_power.clip(lower=self.on_threshold, upper=self.max_power))
 
         # Any data modification or adjustment should take place before the normalization
@@ -190,16 +190,15 @@ class TimeSeriesDataGenerator(Sequence):
         appliance_power = appliance_power.values.reshape(-1, 1)
         aggregated = aggregated.values.reshape(-1, 1)
 
-        # MLM masking
-        if self.wandb_config.mlm_mask:
-            mask = self.apply_mask(aggregated, float(self.masking_portion), float(self.wandb_config.mask_token))
+        masked_aggregated, mask = self.apply_mask(aggregated, float(self.masking_portion),
+                                                  float(self.wandb_config.mask_token))
 
-        return aggregated, appliance_power, mask
+        return masked_aggregated, appliance_power, mask
 
     def apply_mask(self, aggregated, masking_portion, masking_token):
         """
-        Replace a given percentage of the aggregated array with a specific masking value. The masked positions are the
-        ones to be considered for loss computation in the scope of MLM - Masked Learning Model.
+        Replace a given percentage of the aggregated array with a specific masking value.
+        The masked positions are the ones to be considered for loss computation in MLM.
 
         Parameters:
         - aggregated (np.ndarray): Input array, aggregated readings
@@ -207,29 +206,44 @@ class TimeSeriesDataGenerator(Sequence):
         - masking_token (scalar): Value to replace with.
 
         Returns:
-        - np.ndarray: Array with replaced values, i.e., aggregated with masking
+        - np.ndarray: Modified aggregated array with replaced values.
+        - np.ndarray: Mask array indicating masked positions.
         """
+        if not self.wandb_config.mlm_mask or self.wandb_config.model != 'bert':
+            # For other models, return a mask of ones
+            mask = np.ones_like(aggregated)
+            assert aggregated.shape == mask.shape, "Shape mismatch between aggregated and mask"
+            return aggregated, mask
 
-        if self.wandb_config.model != 'bert':
-            # For any other model, all positions are to be used and no masking applies
-            return np.full(aggregated.shape, True)
-
-        # Calculate the number of elements to replace
-        num_elements = len(aggregated)
+        # Flatten the aggregated array for easier manipulation
+        flat_aggregated = aggregated.ravel()
+        num_elements = flat_aggregated.size
         num_to_replace = int(num_elements * masking_portion)
 
-        # Randomly choose indices for replacement
+        # Handle edge cases
+        if num_to_replace <= 0:
+            mask = np.ones_like(aggregated)
+            assert aggregated.shape == mask.shape, "Shape mismatch between aggregated and mask"
+            return aggregated, mask
+
+        # Generate random indices for replacement
         replace_indices = np.random.choice(num_elements, size=num_to_replace, replace=False)
 
-        # Create a boolean mask
-        mask = np.empty(num_elements, dtype=bool)
-        mask.fill(False)
-        mask[replace_indices] = True
+        # Initialize a flat mask array and mark replacement indices
+        flat_mask = np.zeros_like(flat_aggregated)
+        flat_mask[replace_indices] = 1.0
 
-        # Replace the values
-        aggregated[replace_indices] = masking_token
+        # Apply the masking token at the chosen indices
+        flat_aggregated[replace_indices] = masking_token
 
-        return mask
+        # Reshape back to the original aggregated shape
+        modified_aggregated = flat_aggregated.reshape(aggregated.shape)
+        mask = flat_mask.reshape(aggregated.shape)
+
+        # Final assertion to ensure shapes match
+        assert modified_aggregated.shape == mask.shape, "Shape mismatch between modified_aggregated and mask"
+
+        return modified_aggregated, mask
 
     def _count_samples(self):
         """
@@ -308,14 +322,16 @@ class TimeSeriesDataGenerator(Sequence):
             except StopIteration:
                 # Reset the generator and fetch the next batch
                 self.data_generator = self._data_generator()
-                x_m, y = next(self.data_generator)
+                x, y, m = next(self.data_generator)
 
                 if self.wandb_config.model == 'seq2p':
                     midpoint = len(y) // 2
                     y = y[midpoint]
 
-                batch_x.append(x_m)
+                batch_x.append(x)
                 batch_y.append(y)
                 batch_m.append(m)
 
-        return np.array(batch_x), np.array(batch_y), np.array(batch_m)
+        if self.wandb_config.model == 'bert':
+            return np.array(batch_x), np.array(batch_y), np.array(batch_m)
+        return np.array(batch_x), np.array(batch_y)

src/model/bert4nilm.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import Model, layers
 
-from src.custom.loss.bert4nilm import LossFunction
 from .custom.layers import LearnedL2NormPooling, TransformerBlock
 
 
@@ -10,7 +9,6 @@ class BERT4NILM(Model):
     def __init__(self, config, is_training):
         super(BERT4NILM, self).__init__()
         self.config = config
-        self.loss_function = LossFunction(config)
 
         # Embedding module with convolutional layer
         self.conv1d = layers.Conv1D(
@@ -76,39 +74,38 @@ class BERT4NILM(Model):
         x = self.dense1(x)
         x = self.dense2(x)
 
-        # Ensure output is between 0 and 1 TODO: Is that correct?
+        # Ensure output is between 0 and 1
         predictions = tf.clip_by_value(x, 1, self.config['appliance_max_power'])
 
         return predictions
 
     def train_step(self, data):
-        # Unpack the data
         aggregated, y_true, mask = data
 
-        try:
-            with tf.GradientTape() as tape:
-                # Forward pass
-                predictions = self(aggregated, training=True)
-                # Compute loss with explicit mask parameter
-                loss = self.loss_function(y_true, predictions, mask)
+        with tf.GradientTape() as tape:
+            # Forward pass
+            predictions = self(aggregated, training=True)
 
-            # Compute gradients
-            gradients = tape.gradient(loss, self.trainable_variables)
+            # Adjust mask to match y_true/pred dimensions if necessary
+            if len(mask.shape) < len(y_true.shape):
+                mask = tf.expand_dims(mask, axis=-1)
 
-            # Apply gradients
-            self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+            # Apply mask to calculate loss based only on the masked positions
+            bool_mask = tf.cast(mask, tf.bool)
+            y_true_masked = tf.boolean_mask(y_true, bool_mask)
+            predictions_masked = tf.boolean_mask(predictions, bool_mask)
 
-            # Update metrics
-            self.compiled_metrics.update_state(y_true, predictions)
+            # Compute loss
+            loss = self.compiled_loss(y_true_masked, predictions_masked)
 
-            # Update metrics (includes the metric that tracks the loss)
-            for metric in self.metrics:
-                if metric.name == "loss":
-                    metric.update_state(loss)
-                else:
-                    metric.update_state(y_true, predictions)
-            # Return a dict mapping metric names to current value
-            return {m.name: m.result() for m in self.metrics}
-        except Exception as e:
-            print(f"Error in train_step: {e}")
-            return {m.name: m.result() for m in self.metrics}
+        # Compute gradients
+        gradients = tape.gradient(loss, self.trainable_variables)
+
+        # Apply gradients
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+        # Update metrics
+        self.compiled_metrics.update_state(y_true_masked, predictions_masked)
+
+        # Return metrics
+        return {m.name: m.result() for m in self.metrics}

src/train.py
@@ -34,11 +34,18 @@ dataset = DataSet(path_to_dataset)
 training_buildings = [1, 3, 4, 5]
 timeSeries = TimeSeries(dataset, training_buildings, [2], wandb_config)
 
+m_batch = None
 train_gen = timeSeries.getTrainingDataGenerator()
-X_batch, y_batch, m_batch = train_gen[0]
+if wandb_config.model == 'bert':
+    X_batch, y_batch, m_batch = train_gen[0]
+else:
+    X_batch, y_batch = train_gen[0]
 
 # Ensure the shapes match
-print(f"Sample batch shapes - X: {X_batch.shape}, y: {y_batch.shape}")
+if wandb_config.model == 'bert':
+    print(f"Sample batch shapes - X: {X_batch.shape}, y: {y_batch.shape}, m: {m_batch.shape}")
+else:
+    print(f"Sample batch shapes - X: {X_batch.shape}, y: {y_batch.shape}")
 print("Sample statistics:")
 print(f"X mean: {np.mean(X_batch)}, std: {np.std(X_batch)}")
 print(f"y mean: {np.mean(y_batch)}, std: {np.std(y_batch)}")